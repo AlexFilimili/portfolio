2adbcac06e1a7c08f39bc9cf5b84248b783207ad
AlexFili <RootyRoots1507@gmail.com>
2023-02-12 12:47:46 +0300

    block 2 is ready

---
+++ dist/js/script.js
@@ -0,0 +1,15 @@
+const 
+    hamburger = document.querySelector('.humburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+
+    
